44e240939ba3b6922f07485d0e1a335bb61c286b
manish singh <38642903+manishsingh7@users.noreply.github.com>
2021-11-28 10:14:30 +0530

    Add files via upload

---
+++ assignments/favourite_movie/favourite_movie.js
@@ -0,0 +1,15 @@
+const movies = [];
+
+function favouriteMovie(operation, movie) {
+    // Write your code here
+    if(operation == "add") {
+        movies.push(movie);
+    }
+    if(operation == "remove") {
+        movies.pop();
+    }
+    return movies;
+
+}
+
+module.exports = favouriteMovie;
